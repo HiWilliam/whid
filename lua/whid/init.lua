344cea324c0acbb576467c2ccc17860492f24929
wuhao <wuhao5@supcon.com>
2025-07-02 09:58:37 +0800

    update: remove whid cmd

--- lua/whid/init.lua
+++ lua/whid/init.lua
@@ -3,9 +3,7 @@ local M = {}
 
 local todo = require("whid.todo")
 local km = require("whid.common.keymap")
--- Highlight group definitions
-vim.api.nvim_set_hl(0, "WhidHeader", { link = "Number" })
-vim.api.nvim_set_hl(0, "WhidSubHeader", { link = "Identifier" })
+
 local todo_maps = {
 	{ "n", "i", todo.input, { silent = true, noremap = true, nowait = true, buffer = true } },
 	{ "n", "o", todo.input_after, { silent = true, noremap = true, nowait = true, buffer = true } },
@@ -22,10 +20,6 @@ local todo_maps = {
 }
 -- Main WHID command
 function M.setup(cfg)
-	vim.api.nvim_create_user_command("Whid", function()
-		require("whid.whid"):whid()
-	end, {})
-
 	vim.api.nvim_create_user_command("TodoList", function()
 		todo.setup({
 			save_file = cfg.save_file or "/root/todos/default.md",
